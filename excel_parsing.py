c759d843bc63e3814ffb64ec2da69a8f65121c08
Devodriq Roberts <Devorb26@gmail.com>
2019-08-13 22:31:33 -0400

    Added code block for change tag list.

--- excel_parsing.py
+++ excel_parsing.py
@@ -1,9 +1,10 @@
 #%%
 import pandas as pd 
 import numpy as np 
+import os
 
 #%%
-file_location = r'\\txt.textron.com\tsv\Projects\Current Projects\100066 Misc-Advanced Concepts\Text_File_Processing_Scripts\sap_auto_part_entry_erin_amerson_move_this\xml_parsing\routes_and_structures_excel\ECO00032-01-V9_Summary_Rpt.xlsx'
+file_location = os.path.join('routes_and_structures_excel','ECO00032-01-V9_Summary_Rpt.xlsx')
 df = pd.read_excel(file_location, skiprows=1)
 
 
@@ -11,6 +12,31 @@ df = pd.read_excel(file_location, skiprows=1)
 df.rename(columns={'Unnamed: 3':'Child Item Decr'}, inplace=True)
 df = df.where(~df.isnull().all(axis=1), df.fillna('BREAK'))
 df
+
+#%%
+item_dict = {}
+item_count = 0
+for index, row in df.iterrows():
+    
+    change_tag_list = ['New Parts', 'Revised Parts', 'Obsolete Parts', 'Technical Publications Parts']
+    if row['Item #'] in change_tag_list:
+        pass
+    elif row['Item #'] == 'BREAK':
+        print()
+        print('New Item Start', index)
+        
+        item_count += 1
+        print()
+        
+    else:
+        items = {col:row[col] for col in df.columns}
+        item_dict[item_count] = items
+
+    print(item_dict)
+        # rs = [{col: row[col]} for col in df.columns]
+        # print(len(rs))
+        # print(rs)
+
 
 #%%
 
